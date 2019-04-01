3fed538509f163653d31514c87b5f36c46d9f7de
Christopher Magnuson <cwmagnus0@gmail.com>
2019-04-01 00:35:21 -0400

    Added progress circle

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
         <title>Chris Magnuson - Home</title>
 
         <link rel="stylesheet" href="css/home.css">
+        <link rel="stylesheet" href="css/progress.css">
     </head>
     <body>
         <div class="about-me">
@@ -25,54 +26,111 @@
 
         <?php include("../include/navigation.php"); ?>
 
-        <div class="container content">
-            <h1 class="text-light center-md">Featured Projects</h1>
-            <div class="row">
-                <div class="col-lg-8">
-                    <div id="portfolioCarouselHome" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100" src="https://via.placeholder.com/850x500" alt="First slide">
-                                <div class="carousel-caption d-none d-md-block">
-                                    <h1 class="text-dark">ERC Training Application</h1>
-                                    <p class="text-dark">Training application made for the Air Force Research Laboratory</p>
+        <div class="content" id="content">
+           <div class="container">
+                <h1 class="text-light center-md">Featured Projects</h1>
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div id="portfolioCarouselHome" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="https://via.placeholder.com/850x500" alt="First slide">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h1 class="text-dark">ERC Training Application</h1>
+                                        <p class="text-dark">Training application made for the Air Force Research Laboratory</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row pt-4">
+                            <div class="col-6 p-3">
+                                <img class="d-block w-100" src="https://via.placeholder.com/300">
+                            </div>
+                            <div class="col-6 p-3">
+                                <img class="d-block w-100" src="https://via.placeholder.com/300">
+                            </div>
+                            <div class="col-6 p-3">
+                                <img class="d-block w-100" src="https://via.placeholder.com/300">
+                            </div>
+                            <div class="col-6 p-3">
+                                <img class="d-block w-100" src="https://via.placeholder.com/300">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-footer">
+                    <p class="text-light">Want to see all my projects?</p>
+                    <a type="button" class="btn btn-outline-light" href="/portfolio.php">View Portfolio</a>
+                </div>
+            </div>
+
+            <div class="contrast-box skills" id="skill">
+                <div class="container">
+                    <h1 class="text-light center-md pt-3 pb-1">Skills</h1>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <p class="text-light">
+
+                            </p>
+                        </div>
+                        <div class="col-md-9">
+                            <div class="row">
+                                <div class="col-3">
+                                    <div class="progress brown">
+                                        <span class="progress-left">
+                                            <span class="progress-bar progress-bar-left-100"></span>
+                                        </span>
+                                        <span class="progress-right">
+                                            <span class="progress-bar progress-bar-right-100"></span>
+                                        </span>
+                                        <div class="progress-value">C#</div>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="progress brown">
+                                        <span class="progress-left">
+                                            <span class="progress-bar progress-bar-left-80"></span>
+                                        </span>
+                                        <span class="progress-right">
+                                            <span class="progress-bar progress-bar-right-80"></span>
+                                        </span>
+                                        <div class="progress-value">ASP.NET</div>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="progress brown">
+                                        <span class="progress-left">
+                                            <span class="progress-bar progress-bar-left-90"></span>
+                                        </span>
+                                        <span class="progress-right">
+                                            <span class="progress-bar progress-bar-right-90"></span>
+                                        </span>
+                                        <div class="progress-value">C++</div>
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="progress brown">
+                                        <span class="progress-left">
+                                            <span class="progress-bar progress-bar-left-70"></span>
+                                        </span>
+                                        <span class="progress-right">
+                                            <span class="progress-bar progress-bar-right-70"></span>
+                                        </span>
+                                        <div class="progress-value">SQL</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="row pt-4">
-                        <div class="col-6 p-3">
-                            <img class="d-block w-100" src="https://via.placeholder.com/300">
-                        </div>
-                        <div class="col-6 p-3">
-                            <img class="d-block w-100" src="https://via.placeholder.com/300">
-                        </div>
-                        <div class="col-6 p-3">
-                            <img class="d-block w-100" src="https://via.placeholder.com/300">
-                        </div>
-                        <div class="col-6 p-3">
-                            <img class="d-block w-100" src="https://via.placeholder.com/300">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="portfolio-footer">
-                <p class="text-light">Want to see all my projects?</p>
-                <a type="button" class="btn btn-outline-light" href="/portfolio.php">View Portfolio</a>
-            </div>
-        </div>
-
-        <div class="contrast-box">
-            <div class="container">
-                <h1 class="text-light center-md pt-3 pb-1">Skills</h1>
             </div>
         </div>
 
         <?php include("../include/footer.php"); ?>
 
         <?php include("../include/scripts.php"); ?>
-        <script src="js/sticky.js"></script>
+        <script src="js/scroll.js"></script>
     </body>
 </html>
--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,15 +1,18 @@
 // Set scroll callback
 window.onscroll = function() { stickyScroll() };
 
-// Get the navbar and the sticky threshold
+// Get the navbar, content, and the sticky threshold
 var navbar = document.getElementById("navbar");
+var content = document.getElementById("content");
 var stickyThreshold = navbar.offsetTop;
 
-// Make the navbar sticky if scrolled past
+// Sticky scroll callback
 function stickyScroll() {
     if (window.pageYOffset >= stickyThreshold) {
         navbar.classList.add("sticky");
+        content.classList.add("sticky-content");
     } else {
         navbar.classList.remove("sticky");
+        content.classList.remove("sticky-content");
     }
 }
