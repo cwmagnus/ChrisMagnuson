4bb79fb45679f4ae4d8065573a20ad8d2ba04c8a
Chris Magnuson <cwmagnus0@gmail.com>
2019-03-31 21:35:36 -0400

    Added footer

---
+++ include/footer.php
@@ -0,0 +1,32 @@
+<footer class="footer text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+
+            </div>
+            <div class="col-md-4">
+                <h2 class="text-light center-md">Links</h2>
+                <ul class="list-inline">
+                    <li>
+                        <a class="light-link" href="">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a class="light-link" href="">Github</a>
+                    </li>
+                    <li>
+                        <a class="light-link" href="">Resume</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+<div class="copyright py-4 text-center text-white">
+    <div class="container">
+        <small>Copyright &copy; Chris Magnuson 2019</small>
+    </div>
+</div>
